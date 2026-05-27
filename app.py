3e83e2d6034c0f981de9cd92b53fb7afdc84f341
JPMC <jp.martinez.cordeiro@utexas.edu>
2026-05-27 11:37:03 -0500

    improving formatting

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with st.container():
     
     with col1:
         st.markdown("# Juan Pablo Martínez Cordeiro")
-        st.markdown("#Computational Scientist | Mechanical Engineer")
+        st.markdown("## Computational Scientist | Mechanical Engineer")
         st.markdown("### Chasing curiosity and building scalable, rigorous computational models along the way.")
         st.write("""
         __Hi, I'm Juan Pablo Martínez Cordeiro__, a computational scientist and mechanical engineer taking the next steps in my career 
